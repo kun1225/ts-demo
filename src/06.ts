1f00181ded945bfbe3e795deb43256408525f697
kun1225 <kun881225@gmail.com>
2026-04-01 18:21:06 +0800

    06

---
+++ src/06.ts
@@ -0,0 +1,13 @@
+// *** Literal ***
+
+let message: "hello" = "hello";
+
+let theme: "light" | "dark" = "light";
+
+theme = "dark";
+
+let httpCode: 200 | 404 | 500 = 200;
+
+httpCode = 404;
+
+let isAdmin: true = true;
