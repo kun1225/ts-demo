9949a6db58a26c88a7e4a90a3fe560caa5d4433e
kun1225 <kun881225@gmail.com>
2026-04-02 16:10:26 +0800

    update docs

---
+++ src/12.ts
@@ -0,0 +1,11 @@
+// *** 一、Props 型別 ***
+
+// *** 二、Children 型別 ***
+
+// *** 三、`useState` 型別應用 ***
+
+// *** 四、`useContext` ***
+
+// *** 五、`useRef` 與 React 事件型別 ***
+
+// *** 六、`React.CSSProperties` ***
---
+++ src/14.ts
@@ -0,0 +1,11 @@
+// *** 一、什麼是型別收窄（Type Narrowing）？ ***
+
+// *** 二、內建的型別守衛 ***
+
+// *** 三、Discriminated Union（可辨識聯集） ***
+
+// *** 四、自訂型別守衛函式（Type Predicate） ***
+
+// *** 五、Assertion Functions ***
+
+// *** 六、自訂守衛的注意事項 ***
